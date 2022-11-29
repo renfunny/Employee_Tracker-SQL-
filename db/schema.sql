9c45259bf160c320087952bf7a98c58600310183
Renato Cordova <ren99cordova@gmail.com>
2022-11-29 14:07:31 -0500

    added libraries needed and started the schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    department_name VARCHAR(30), 
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL,
+);
